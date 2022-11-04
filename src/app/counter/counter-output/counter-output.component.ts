0b0efa34ef64108b68e64f0c391c448af02f7d13
mohamed <mohamed.ali@atayen.us>
2022-11-04 15:29:17 +0100

    without ngrx counter

---
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-output',
+  templateUrl: './counter-output.component.html',
+  styleUrls: ['./counter-output.component.css']
+})
+export class CounterOutputComponent implements OnInit {
+
+  @Input() counter: any;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
